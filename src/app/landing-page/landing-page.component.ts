89c9d522a169775d91b93440b79e5bbebdba3f9d
ArtinRezaee <artin.anzabee@gmail.com>
2018-07-09 18:55:13 -0600

    New Accepted Papers section added

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -13,23 +13,33 @@ export class LandingPageComponent {
   "	Mapping RE elements from the SWEBOK (Software Engineering Body Of Knowledge) to RE curricula",
   "	Identifying and incorporating specific RE related topics into the general curriculum and/or software engineering courses",
   "	Curriculum for industrial training programs "
-  ]
+  ];
 
   techniques: String[] = ["	Creative methods for teaching stakeholder identification, requirements elicitation, negotiation and consensus building, requirements writing, and other critical RE skills",
   "	Specific tools, exercises, and assignments developed to support RE skills training"
-  ]
+  ];
 
   Assessments: String [] = ["	Which assessment method to use: exam, test, case study, essay, report, presentation, or something else?", 
   "	Strategies for assessment of learning soft skills",
   "	What should students be able to do as a result of learning RE? ",
   "	Methods of objectively measuring assessments "
-  ]
+  ];
 
   methods: String [] = ["	Survey results related to topics such as the effectiveness of teaching methods, RE skills needed to be effective in industry, skill mismatches between graduating students and industry needs etc.", 
   "	Studies into the effectiveness of requirements engineering educational practices ",
   "	Studies on different pedagogic approaches to improve the learning outcomes",
   "	Experience reports including industrial training and university level curriculum"
-  ]
+  ];
+
+  papers: {}[] = [
+    {authors: "Bernd Westphal", title: "An Undergraduate Requirements Engineering Curriculum with Formal Methods"},
+    {authors: "Jennifer Horkoff", title: "The Influences of Agile Methods in Requirements Engineering Pedagogy"},
+    {authors: "Kim Hertz and Paola Spoletini", title: "Are Requirements Engineering Courses Covering what Industry Needs? A Preliminary Analysis of the U.S. Situation"},
+    {authors: "Itzel Morales-Ramirez and Luis Alva-Martinez", title: "Requirements Analysis Skills: How to Train Practitioners?"},
+    {authors: "Antonio Lopez-Lorca, Rachel Burrows and Leon Sterling", title: "Teaching Goal Models in Agile Requirements Engineering"}
+  ];
+
+  displayedColumns: string[] = ["Authors", "Title"];
 
   mobileQuery: MediaQueryList;
   private _mobileQueryListener: () => void;
